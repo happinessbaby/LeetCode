fd817624f1b0b4449d4a9393f2f38f7fdc8c1f27
happinessbaby <88567655+happinessbaby@users.noreply.github.com>
2022-05-14 11:55:51 -0400

    Time: 577 ms (22.00%), Space: 0B (100.00%) - LeetHub

---
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select temp.customer_number from (select customer_number, count(*) as "order_count" from Orders group by customer_number order by order_count desc limit 1) temp
