a8d12773126b399be7b99f12186838c5a2b804bb
happinessbaby <88567655+happinessbaby@users.noreply.github.com>
2022-05-27 13:07:20 -0400

    Time: 80 ms (26.20%), Space: 14.1 MB (90.57%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -8,16 +8,40 @@ class Solution:
         #         stack.pop()
         #     else:
         #         stack.append(s[i])
+        # for i in range(len(s)):
+        #     if s[i]=="(":
+        #         stack.append(i)
+        #     else:
+        #         stack.pop()
+        #         if len(stack)==0:
+        #             stack.append(i)
+        #         else:
+        #             max_len = max(max_len, i-stack[-1])
+        # return max_len
+        right = 0
+        left = 0
+        max_len = 0
         for i in range(len(s)):
             if s[i]=="(":
-                stack.append(i)
+                left+=1
             else:
-                stack.pop()
-                if len(stack)==0:
-                    stack.append(i)
-                else:
-                    max_len = max(max_len, i-stack[-1])
+                right+=1
+            if left==right:
+                max_len = max(max_len, 2*left)
+            elif right>=left:
+                left=right=0
+        left=right=0
+        for j in range(len(s)-1, -1, -1):
+            if s[j]=="(":
+                left+=1
+            else:
+                right+=1
+            if left==right:
+                max_len = max(max_len, 2*left)
+            elif left>=right:
+                left=right=0
         return max_len
+            
                 
             
                 
